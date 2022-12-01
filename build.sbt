a48474a27c6c190231409094f0c560bc17f8b922
Shirali Shah <shiralishah1198@gmail.com>
2022-12-01 13:58:47 -0500

    initial commit

---
+++ build.sbt
@@ -0,0 +1,3 @@
+name := "aoc"
+version := "0.1"
+scalaVersion := "2.13.10"
---
+++ src/main/scala/aoc22/Day01.scala
@@ -0,0 +1,6 @@
+package aoc22
+
+object Day01 extends App {
+  val input = scala.io.Source.fromResource("aoc22/day01.txt").getLines().toSeq
+  // TODO
+}
