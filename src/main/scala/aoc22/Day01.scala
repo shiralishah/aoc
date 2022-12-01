f8c1459d0d4942224b859c19af92250398920513
Shirali Shah <shiralishah1198@gmail.com>
2022-12-01 14:50:46 -0500

    day 01 complete!

--- src/main/scala/aoc22/Day01.scala
+++ src/main/scala/aoc22/Day01.scala
@@ -2,5 +2,14 @@ package aoc22
 
 object Day01 extends App {
   val input = scala.io.Source.fromResource("aoc22/day01.txt").getLines().toSeq
-  // TODO
+  val (parsedCals, lastTotal) = input.foldLeft((Seq.empty[Long], 0L)){ case((calories, total), line) =>
+    line match {
+      case "" => (calories :+ total, 0L)
+      case other => (calories, total + other.toLong)
+    }
+  }
+  val elfCals = (parsedCals :+ lastTotal).sortBy(-_)
+
+  println(elfCals.head)
+  println(elfCals.take(3).sum)
 }
