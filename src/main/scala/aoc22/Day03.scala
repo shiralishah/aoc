ff7adbe6226b31cc3992352ad9af5dbcbb5e31ea
Shirali Shah <shiralishah1198@gmail.com>
2022-12-03 01:01:48 -0500

    Advent of code day 3

---
+++ src/main/scala/aoc22/Day03.scala
@@ -0,0 +1,22 @@
+package aoc22
+
+object Day03A extends App {
+  val input = scala.io.Source.fromResource("aoc22/day03.txt").getLines().toSeq
+  val rucksacks = input.map {
+    rucksack =>
+      rucksack.splitAt(rucksack.length/2)
+  }
+  val commons = rucksacks.map( rucksack => rucksack._1.intersect(rucksack._2).head)
+  val priorities = commons.map(item => if (item.isLower) item.toInt - 96 else item.toInt-38).sum
+  println(priorities)
+}
+
+object Day03B extends App {
+  val input = scala.io.Source.fromResource("aoc22/day03.txt").getLines().sliding(3, 3).toSeq
+  val commons = input.map {
+    rucksackGroup =>
+      rucksackGroup(0).intersect(rucksackGroup(1)).intersect(rucksackGroup(2)).head
+  }
+  val priorities = commons.map(item => if (item.isLower) item.toInt - 96 else item.toInt-38).sum
+  println(priorities)
+}
