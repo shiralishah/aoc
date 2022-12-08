971bef03f388a903378b72109f5f30138a64175b
Shirali Shah <shiralishah1198@gmail.com>
2022-12-08 10:34:09 -0500

    fixed transpose

--- src/main/scala/aoc22/Day08.scala
+++ src/main/scala/aoc22/Day08.scala
@@ -1,16 +1,19 @@
 package aoc22
 
+import aoc22.Day08A.input
+
 object Day08A extends App {
   val input  = scala.io.Source.fromResource("aoc22/day08.txt").getLines().map(_.toCharArray.map(_.asDigit)).toArray
   val width  = input.head.length
   val height = input.length
   val base   = 2 * (width - 1) + 2 * (height - 1)
+  val inverse = input.transpose
 
   val visibility = (1 until (height - 1)).flatMap { y =>
     (1 until (width - 1)).map { x =>
       val h             = input(y)(x)
       val (left, right) = input(y).splitAt(x)
-      val (up, down)    = input.transpose(identity)(x).splitAt(y)
+      val (up, down)    = inverse(x).splitAt(y)
       !(left.max >= h && right.tail.max >= h && up.max >= h && down.tail.max >= h)
     }
   }
@@ -24,12 +27,13 @@ object Day08B extends App {
   val input  = scala.io.Source.fromResource("aoc22/day08.txt").getLines().map(_.toCharArray.map(_.asDigit)).toArray
   val width  = input.head.length
   val height = input.length
+  val inverse = input.transpose
 
   val maxScore = (0 until height).flatMap { y =>
     (0 until width).map { x =>
       val t             = input(y)(x)
       val (left, right) = input(y).zipWithIndex.splitAt(x)
-      val (up, down)    = input.transpose(identity)(x).zipWithIndex.splitAt(y)
+      val (up, down)    = inverse(x).zipWithIndex.splitAt(y)
       val leftMax       = left.findLast(_._1 >= t).map(_._2).getOrElse(left.headOption.map(_._2).getOrElse(x))
       val rightMax      = right.tail.find(_._1 >= t).map(_._2).getOrElse(right.tail.lastOption.map(_._2).getOrElse(x))
       val upMax         = up.findLast(_._1 >= t).map(_._2).getOrElse(up.headOption.map(_._2).getOrElse(y))
